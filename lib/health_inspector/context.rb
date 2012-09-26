1ce78f4250ad35c1b2a95230c6f456ce6c64c1fc
Adam Sinnett <sinneta@webtrends.com>
2012-09-26 13:53:28 -0700

    Removed knife usage, switched to entirely using Chef::REST

--- lib/health_inspector/context.rb
+++ lib/health_inspector/context.rb
@@ -2,10 +2,6 @@ require "chef/config"
 
 module HealthInspector
   class Context < Struct.new(:repo_path, :config_path)
-    def knife_command(subcommnad)
-      `cd #{repo_path} && knife #{subcommnad} -c #{config_path}`
-    end
-
     def cookbook_path
       Array( config.cookbook_path )
     end

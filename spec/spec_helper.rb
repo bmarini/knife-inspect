bfc62d6aeea6d321d20dbc836fc092c09e4507f8
Ben Marini <bmarini@gmail.com>
2012-11-01 13:13:08 -0700

    Pass in a useless context obj in specs that isnt used yet

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,7 @@ require 'health_inspector'
 
 module HealthInspector::SpecHelpers
   def health_inspector_context
-    @health_inspector_context ||= begin
-      repo_path = File.expand_path("../chef-repo", __FILE__)
-
-      HealthInspector::Context.new( repo_path, File.join(repo_path, ".chef/knife.rb") ).tap do |context|
-        context.configure
-      end
-    end
+    @health_inspector_context ||= HealthInspector::Context.new(nil)
   end
 end
 

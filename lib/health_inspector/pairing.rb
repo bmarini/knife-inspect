32d4bd3be1e83e7e223ac9be9f431feb2cd6e516
Kirt Fitzpatrick <kirt.fitzpatrick@gmail.com>
2013-01-28 14:12:40 -0800

    refactored the hash_diff method to work for more cases.  Added tests for same.

--- lib/health_inspector/pairing.rb
+++ lib/health_inspector/pairing.rb
@@ -37,16 +37,64 @@ module HealthInspector
       self.methods.grep(/^validate_/).each { |meth| send(meth) }
     end
 
+
     def hash_diff(original, other)
-      (original.keys + other.keys).uniq.inject({}) do |memo, key|
-        unless original[key] == other[key]
-          if original[key].kind_of?(Hash) && other[key].kind_of?(Hash)
-            memo[key] = hash_diff(original[key], other[key])
-          else
-            memo[key] = {"server" => original[key],"local" => other[key]}
+      recursive_diff(stringify_hash_keys(original), stringify_hash_keys(other))
+    end
+    
+    def stringify_hash_keys(original)
+      original.keys.inject({}) do |original_strkey, key|
+        original_strkey[key.to_s] = stringify_item(original[key])
+        original_strkey
+      end
+    end
+    
+    def stringify_item(item)
+      if item.kind_of?(Hash)
+        stringify_hash_keys(item)
+      elsif item.kind_of?(Array)
+        mapped = item.map {|array_item| stringify_item(array_item) }
+        mapped.sort {|a,b| heterogeneous_sort(a,b)}
+      else # must be a string
+        item
+      end
+    end
+    
+    def heterogeneous_sort(a, b)
+      a_class = a.class.to_s
+      b_class = b.class.to_s
+      
+      if a.kind_of?(Hash) && b.kind_of?(Hash)
+        a.keys.sort.join <=> b.keys.sort.join
+      elsif a_class == b_class
+        return a <=> b
+      else
+        return a_class <=> b_class
+      end
+    end
+  
+    def recursive_diff(original, other)
+      if original.kind_of?(Hash) && other.kind_of?(Hash)
+        (original.keys + other.keys).uniq.inject({}) do |memo, key|
+          unless original[key] == other[key]
+            if original[key].kind_of?(Hash) && other[key].kind_of?(Hash)
+              diff = recursive_diff(original[key], other[key])
+              memo[key] = diff unless diff.empty?
+            elsif original[key].kind_of?(Array) && other[key].kind_of?(Array) && original.size == other.size
+              all_diffs = []
+              original.each_index do |idx|
+                diff = recursive_diff(original[idx], other[idx])
+                all_diffs << diff unless diff.nil? || diff.empty?
+              end
+              memo[key] = all_diffs
+            else
+              memo[key] = {"server" => original[key], "local" => other[key]}
+            end
           end
+          memo
         end
-        memo
+      else
+        return original == other
       end
     end
   end

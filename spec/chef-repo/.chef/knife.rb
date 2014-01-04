d934cd146a00ec72142fd4509cd4e85d026b6c04
Greg Karékinian <greg@karekinian.com>
2014-01-04 15:24:20 +0300

    Remove chef-repo from the spec directory
    It's not used anywhere

--- spec/chef-repo/.chef/knife.rb
+++
@@ -1,7 +0,0 @@
-chef_repo_dir = File.expand_path( "../", File.dirname(__FILE__) )
-
-client_key               "#{chef_repo_dir}/.chef/client.pem"
-log_level                :info
-log_location             STDOUT
-cache_type               "BasicFile"
-cookbook_path            [ "#{chef_repo_dir}/cookbooks" ]

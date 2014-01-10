02a96291f15ee039ca3746c96548733e2d183a34
Greg Karékinian <greg@karekinian.com>
2014-01-10 12:20:35 +0300

    Still generate the local coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
 if RUBY_VERSION > '1.9'
   require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
   SimpleCov.start do
     add_filter "/spec/"
   end
-
-  require 'coveralls'
-  Coveralls.wear!
 end
 
 require 'rubygems'

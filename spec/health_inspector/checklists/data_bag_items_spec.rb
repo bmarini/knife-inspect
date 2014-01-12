f69867b637967bc48e0ae2f8998f57d60c5c2273
Greg Karékinian <greg@karekinian.com>
2014-01-12 16:03:19 +0300

    Improve rescuing of exceptions in Checklists::DataBagItems
    Also add more specs:
    
    Checklists::DataBagItems#load_item,
    Checklists::DataBagItems#load_item_from_local &
    Checklists::DataBagItemsload_item_from_server

--- spec/health_inspector/checklists/data_bag_items_spec.rb
+++ spec/health_inspector/checklists/data_bag_items_spec.rb
@@ -36,4 +36,93 @@ describe HealthInspector::Checklists::DataBagItems do
       ]
     end
   end
+
+  describe '#load_item(name)' do
+    let :name do
+      'data_bag_one/one'
+    end
+
+    let :item do
+      checklist.load_item name
+    end
+
+    it 'instanciates a DataBagItem pairing for that item' do
+      expect(checklist).to receive(:load_item_from_local).
+        with(name).and_return name
+      expect(checklist).to receive(:load_item_from_server).
+        with(name).and_return name
+
+      expect(item).to be_a(HealthInspector::Checklists::DataBagItem)
+      expect(item.name).to eq(name)
+      expect(item.server).to eq(name)
+    end
+  end
+
+  describe '#load_item_from_local(name)' do
+    context 'when the data bag item exists' do
+      let :name do
+        'data_bag_one/one'
+      end
+
+      it 'returns a parsed version of the data bag item' do
+        expect(checklist.load_item_from_local(name)).
+          to eq({ 'id' => 'one', 'some' => 'key' })
+      end
+    end
+
+    context 'when the data bag item does not exist' do
+      let :name do
+        'data_bag_one/non_existent'
+      end
+
+      it 'returns nil' do
+        expect(checklist.load_item_from_local(name)).to be_nil
+      end
+    end
+
+    context 'when the data bag item is not valid json' do
+      let :name do
+        'data_bag_two/two'
+      end
+
+      it 'returns nil' do
+        expect(checklist.load_item_from_local(name)).to be_nil
+      end
+    end
+  end
+
+  describe '#load_item_from_server(name)' do
+    context 'when the data bag item exists' do
+      let :name do
+        'data_bag_one/one'
+      end
+
+      let :data_bag_item do
+        double(:raw_data => { 'id' => 'one', 'some' => 'key' })
+      end
+
+      it 'returns a parsed version of the data bag item' do
+        expect(Chef::DataBagItem).to receive(:load).
+          with('data_bag_one', 'one').
+          and_return(data_bag_item)
+        expect(checklist.load_item_from_server(name)).
+          to eq({ 'id' => 'one', 'some' => 'key' })
+      end
+    end
+
+    context 'when the data bag item does not exist' do
+      let :name do
+        'data_bag_one/non_existent'
+      end
+
+      it 'returns nil' do
+        expect(Chef::DataBagItem).to receive(:load).
+          with('data_bag_one', 'non_existent').
+          and_raise(Net::HTTPServerException.new('404 "Object Not Found"',
+                                                 'response'))
+        expect(checklist.load_item_from_server(name)).to be_nil
+      end
+    end
+
+  end
 end

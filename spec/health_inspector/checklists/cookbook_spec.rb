db8ef2e6aae680f1a7b022d1704e61cf7a211ff5
Greg Karékinian <greg@karekinian.com>
2018-04-05 15:46:54 +0200

    Fix the specs on Chef < 13.0

--- spec/health_inspector/checklists/cookbook_spec.rb
+++ spec/health_inspector/checklists/cookbook_spec.rb
@@ -34,15 +34,29 @@ RSpec.describe HealthInspector::Checklists::Cookbook do
 
   it 'detects if an item is the same' do
     cookbook_version = double('Chef::CookbookVersion')
-    allow(cookbook_version).to receive(:files_for).with(:attributes).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:definitions).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:files).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:libraries).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:providers).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:recipes).and_return([{ "path" => "recipes/default.rb", "checksum" => "d41d8cd98f00b204e9800998ecf8427e" }])
-    allow(cookbook_version).to receive(:files_for).with(:root_files).and_return([{"path" => "metadata.rb", "checksum" => "feaa135db12fdc37c3cd155d16f80b5f"}])
-    allow(cookbook_version).to receive(:files_for).with(:resources).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:templates).and_return([])
+    if Gem::Version.new(Chef::VERSION) < Gem::Version.new('13.0.0')
+      allow(cookbook_version).to receive(:manifest).and_return({
+        attributes: [],
+        definitions: [],
+        files: [],
+        libraries: [],
+        providers: [],
+        recipes: [{ "path" => "recipes/default.rb", "checksum" => "d41d8cd98f00b204e9800998ecf8427e" }],
+        root_files: [{"path" => "metadata.rb", "checksum" => "feaa135db12fdc37c3cd155d16f80b5f"}],
+        resources: [],
+        templates: []
+      })
+    else
+      allow(cookbook_version).to receive(:files_for).with(:attributes).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:definitions).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:files).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:libraries).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:providers).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:recipes).and_return([{ "path" => "recipes/default.rb", "checksum" => "d41d8cd98f00b204e9800998ecf8427e" }])
+      allow(cookbook_version).to receive(:files_for).with(:root_files).and_return([{"path" => "metadata.rb", "checksum" => "feaa135db12fdc37c3cd155d16f80b5f"}])
+      allow(cookbook_version).to receive(:files_for).with(:resources).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:templates).and_return([])
+    end
     expect(Chef::CookbookVersion).to receive(:load)
       .with('cookbook_one', '0.0.1')
       .and_return(
@@ -58,15 +72,30 @@ RSpec.describe HealthInspector::Checklists::Cookbook do
 
   it 'detects if an item is not on the server' do
     cookbook_version = double('Chef::CookbookVersion')
-    allow(cookbook_version).to receive(:files_for).with(:attributes).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:definitions).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:files).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:libraries).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:providers).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:recipes).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:root_files).and_return([{"path" => "metadata.rb", "checksum" => "feaa135db12fdc37c3cd155d16f80b5f"}])
-    allow(cookbook_version).to receive(:files_for).with(:resources).and_return([])
-    allow(cookbook_version).to receive(:files_for).with(:templates).and_return([])
+
+    if Gem::Version.new(Chef::VERSION) < Gem::Version.new('13.0.0')
+      allow(cookbook_version).to receive(:manifest).and_return({
+        attributes: [],
+        definitions: [],
+        files: [],
+        libraries: [],
+        providers: [],
+        recipes: [],
+        root_files: [{"path" => "metadata.rb", "checksum" => "feaa135db12fdc37c3cd155d16f80b5f"}],
+        resources: [],
+        templates: []
+      })
+    else
+      allow(cookbook_version).to receive(:files_for).with(:attributes).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:definitions).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:files).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:libraries).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:providers).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:recipes).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:root_files).and_return([{"path" => "metadata.rb", "checksum" => "feaa135db12fdc37c3cd155d16f80b5f"}])
+      allow(cookbook_version).to receive(:files_for).with(:resources).and_return([])
+      allow(cookbook_version).to receive(:files_for).with(:templates).and_return([])
+    end
     expect(Chef::CookbookVersion).to receive(:load)
       .with('cookbook_one', '0.0.1')
       .and_return(
